0dd5cfe863aa49abe69f66ecef106529598c4f08
PFLeget <leget@stanford.edu>
2019-11-28 11:19:38 +0100

    continue to write config

--- gastrometry/__init__.py
+++ gastrometry/__init__.py
@@ -1,10 +1,9 @@
-# init file of astrometry
+# init file of gastrometry
+
+from . import plotting
 
 from .math_toolbox import biweight_median, biweight_mad
 from .math_toolbox import median_check_finite
 from .math_toolbox import vcorr, xiB
 from .math_toolbox import return_var_map
 
-from .input import read_input
-
-from . import plotting
--- gastrometry/config.py
+++ gastrometry/config.py
@@ -1,67 +1,96 @@
 
-
 def gastrogp(config, read_input_only=False, 
-             interp_only=False, write_output_only=False):
+             interp_only=False, write_output=False):
     """
     To do.
     """
     import glob
     import numpy as np
+    from .input import read_input
 
-    for key in ['output', 'hyper']:
-        if key not in config:
-            raise ValueError("%s field is required in config dict"%key)
-    for key in ['file_name']:
-        if key not in config['output']:
-            raise ValueError("%s field is required in config dict output"%key)
+    if not read_input_only & not interp_only:
+        raise ValueError("At least one option should be set to True.")
 
-    for key in ['file_name']:
-        if key not in config['hyper']:
-            raise ValueError("%s field is required in config dict hyper"%key)
+    if read_input_only & interp_only:
+        raise ValueError("Just one option should be set to True.")
 
-    if 'dir' in config['output']:
-        dir = config['output']['dir']
-    else:
-        dir = None
+    if 'output' not in config:
+        raise ValueError("output field is required in config dict")
 
-    if 'bin_spacing' in config['hyper']:
-        bin_spacing = config['hyper']['bin_spacing'] #in arcsec
-    else:
-        bin_spacing = 120. #default bin_spacing: 120 arcsec
+    if read_input_only:
+        if 'input' not in config:
+            raise ValueError("output field is required in config dict")
+        for key in ['directory', 'filt_telescop']:
+            if key not in config['input']:
+                raise ValueError("%s field is required in config dict"%key) 
 
-    if 'statistic' in config['hyper']:
-        if config['hyper']['statistic'] not in ['mean', 'median']:
-            raise ValueError("%s is not a suported statistic (only mean and median are currently suported)"
-                             %config['hyper']['statistic'])
-        else:
-            stat_used = config['hyper']['statistic']
-    else:
-        stat_used = 'mean' #default statistics: arithmetic mean over each bin
+        read_input(input_astrometry=config['input']['directory'],
+                   output=config['output']['directory'],
+                   filt_telescop=config['input']['filt_telescop'])
+    
+    #for key in ['output', 'hyper']:
+    #    if key not in config:
+    #        raise ValueError("%s field is required in config dict"%key)
+    #for key in ['file_name']:
+    #    if key not in config['output']:
+    #        raise ValueError("%s field is required in config dict output"%key)
 
-    if 'params_fitted' in config['hyper']:
-        if type(config['hyper']['params_fitted']) != list:
-            raise TypeError('must give a list of index for params_fitted')
-        else:
-            params_fitted = config['hyper']['params_fitted']
-    else:
-        params_fitted = None
+    #for key in ['file_name']:
+    #    if key not in config['hyper']:
+    #        raise ValueError("%s field is required in config dict hyper"%key)
 
-    if isinstance(config['output']['file_name'], list):
-        psf_list = config['output']['file_name']
-        if len(psf_list) == 0:
-            raise ValueError("file_name may not be an empty list")
-    elif isinstance(config['output']['file_name'], str):
-        file_name = config['output']['file_name']
-        if dir is not None:
-            file_name = os.path.join(dir, file_name)
-        psf_list = sorted(glob.glob(file_name))
-        if len(psf_list) == 0:
-            raise ValueError("No files found corresponding to "+config['file_name'])
-    elif not isinstance(config['file_name'], dict):
-        raise ValueError("file_name should be either a dict or a string")
+    #if 'dir' in config['output']:
+    #    dir = config['output']['dir']
+    #else:
+    #    dir = None
 
-    if psf_list is not None:
-        logger.debug('psf_list = %s',psf_list)
-        npsfs = len(psf_list)
-        logger.debug('npsfs = %d',npsfs)
-        config['output']['file_name'] = psf_list
+    #if 'bin_spacing' in config['hyper']:
+    #    bin_spacing = config['hyper']['bin_spacing'] #in arcsec
+    #else:
+    #    bin_spacing = 120. #default bin_spacing: 120 arcsec
+
+    #if 'statistic' in config['hyper']:
+    #    if config['hyper']['statistic'] not in ['mean', 'median']:
+    #        raise ValueError("%s is not a suported statistic (only mean and median are currently suported)"
+    #                         %config['hyper']['statistic'])
+    #    else:
+    #        stat_used = config['hyper']['statistic']
+    #else:
+    #    stat_used = 'mean' #default statistics: arithmetic mean over each bin
+
+    #if 'params_fitted' in config['hyper']:
+    #    if type(config['hyper']['params_fitted']) != list:
+    #        raise TypeError('must give a list of index for params_fitted')
+    #    else:
+    #        params_fitted = config['hyper']['params_fitted']
+    #else:
+    #    params_fitted = None
+
+    #if isinstance(config['output']['file_name'], list):
+    #    psf_list = config['output']['file_name']
+    #    if len(psf_list) == 0:
+    #        raise ValueError("file_name may not be an empty list")
+    #elif isinstance(config['output']['file_name'], str):
+    #    file_name = config['output']['file_name']
+    #    if dir is not None:
+    #        file_name = os.path.join(dir, file_name)
+    #    psf_list = sorted(glob.glob(file_name))
+    #    if len(psf_list) == 0:
+    #        raise ValueError("No files found corresponding to "+config['file_name'])
+    #elif not isinstance(config['file_name'], dict):
+    #    raise ValueError("file_name should be either a dict or a string")
+
+    #if psf_list is not None:
+    #    logger.debug('psf_list = %s',psf_list)
+    #    npsfs = len(psf_list)
+    #    logger.debug('npsfs = %d',npsfs)
+    #    config['output']['file_name'] = psf_list
+
+if __name__ = '__main__':
+
+    config = {'input':{'directory':'/sps/snls15/HSC/prod.2019-04.dev/dbimage_JLBFUJY/fitastrom_FUINMJY/data/',
+                       'filt_telescop'=['g', 'r', 'r2', 'i', 'i2', 'z', 'y']},
+              'output':{'directory':'/pbs/home/l/leget/sps_lsst/HSC/gp_output'}}
+
+    gastrogp(config, read_input_only=True,
+             interp_only=False, write_output=False)
