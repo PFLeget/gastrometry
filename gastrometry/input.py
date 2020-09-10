37bce9005cfa8dbf0c033e95215c96ec7f26a292
Pierre-Francois Leget <leget@clermont.in2p3.fr>
2020-09-10 11:41:48 +0200

    fixe typo

--- gastrometry/input.py
+++ gastrometry/input.py
@@ -52,7 +52,7 @@ def read_input(input_astrometry='/sps/snls15/HSC/prod.2019-04.dev/dbimage_JLBFUJ
                        'du_err':A[:,12][Filtre],
                        'dv_err':A[:,13][Filtre],
                        'chip_num':A[:,20][Filtre],
-                       'magic_mag':A[:,4][Filtre]
+                       'magic_mag':A[:,4][Filtre],
                        'dic_all':dic_all}
 
                 pickle.dump(dic, pkl_file)
